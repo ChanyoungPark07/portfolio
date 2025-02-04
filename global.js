521e2892f30d10b81bff5595b097ccce84fbbe5f
Chanyoung Park <chadpark2004@gmail.com>
2025-02-03 20:24:56 -0800

    Update files

--- global.js
+++ global.js
@@ -110,7 +110,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <${headingLevel}>${project.title}<${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.year} - ${project.description}</p>
+      <p>${project.year} | ${project.description}</p>
     `;
     containerElement.appendChild(article);
   }
--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
 // Fetch and render project data
 const projects = await fetchJSON('../lib/projects.json');
@@ -6,8 +7,24 @@ const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
 
-
 // Display number of projects
 const projectsTitle = document.querySelector('.projects-title');
 const h1Title = projectsTitle.querySelector('h1')
 h1Title.innerHTML = `${projects.length} Projects`;
+
+// Display Pie Chart
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2, 3, 4, 5, 5];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx))
+})
