c0f0adbc2b75e7210d64bef7ea81965271542eb7
Chanyoung Park <chadpark2004@gmail.com>
2025-02-04 23:02:30 -0800

    Update files

--- projects/projects.js
+++ projects/projects.js
@@ -12,19 +12,59 @@ const projectsTitle = document.querySelector('.projects-title');
 const h1Title = projectsTitle.querySelector('h1')
 h1Title.innerHTML = `${projects.length} Projects`;
 
-// Display Pie Chart
+// Display pie chart
+function renderPieChart(projectsGiven) {
+  let newRolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+  let newData = newRolledData.map(([year, count]) => {
+    return { value: count, label: year };
+  });
+  let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+  let newSliceGenerator = d3.pie().value((d) => d.value);
+  let newArcData = newSliceGenerator(newData);
+  let newArcs = newArcData.map((d) => arcGenerator(d));
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
-let data = [1, 2, 3, 4, 5, 5];
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-let sliceGenerator = d3.pie();
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
-
-arcs.forEach((arc, idx) => {
-    d3.select('svg')
+  let newSVG = d3.select('svg'); 
+  newSVG.selectAll('path').remove();
+  
+  newArcs.forEach((arc, idx) => {
+    newSVG
       .append('path')
       .attr('d', arc)
       .attr('fill', colors(idx))
-})
+  })
+
+  let legend = d3.select('.legend');
+  legend.selectAll('li').remove();
+  newData.forEach((d, idx) => {
+    legend.append('li')
+      .attr('style', `--color:${colors(idx)}`)
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+  })
+
+  if (projectsGiven.length === 0) {
+    legend.style('border', 'none');
+  } else {
+    legend.style('border', 'solid 1px gray');
+  }
+}
+
+// Call this function on page load
+renderPieChart(projects);
+
+// Search query
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('input', (event) => {
+  query = event.target.value;
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
+});
